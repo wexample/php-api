b88293508aa0b482188a32613208b6cc92efe88a
weeger <romain.weeger@gmail.com>
2026-01-12 11:08:51 +0100

    Default client classes

--- src/Common/AbstractApiClient.php
+++ src/Common/AbstractApiClient.php
@@ -6,4 +6,8 @@ namespace Wexample\PhpApi\Common;
 
 abstract class AbstractApiClient extends Client
 {
+    public function requestJson(string $method, string $path, array $options = []): array
+    {
+        return parent::requestJson($method, $path, $options);
+    }
 }
