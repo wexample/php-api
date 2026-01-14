7468051f87bc31b6e1bc9816169fb9033a7f5e2e
weeger <romain.weeger@gmail.com>
2026-01-14 17:01:45 +0100

    Secure id and relations

--- src/Common/AbstractApiEntity.php
+++ src/Common/AbstractApiEntity.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Wexample\PhpApi\Common;
 
+use BadMethodCallException;
+use ReflectionClass;
+
 abstract class AbstractApiEntity
 {
     public function __construct(
@@ -57,5 +60,56 @@ abstract class AbstractApiEntity
     public function setRelationships(array $relationships): void
     {
         $this->relationships = $relationships;
+    }
+
+    public function __call(string $name, array $arguments): mixed
+    {
+        if (preg_match('/^get(.+)SecureId$/', $name, $matches) === 1) {
+            $property = lcfirst($matches[1]) . 'SecureId';
+
+            if (property_exists($this, $property)) {
+                return $this->$property;
+            }
+
+            return null;
+        }
+
+        if (preg_match('/^get(.+)Relationship$/', $name, $matches) === 1) {
+            return $this->findRelationshipByName($matches[1]);
+        }
+
+        throw new BadMethodCallException(sprintf('Method %s::%s does not exist.', static::class, $name));
+    }
+
+    protected function findRelationshipByName(string $name): ?AbstractApiEntity
+    {
+        $normalizedTarget = $this->normalizeRelationshipName($name);
+
+        foreach ($this->relationships as $relationship) {
+            if (! $relationship instanceof AbstractApiEntity) {
+                continue;
+            }
+
+            $shortName = (new ReflectionClass($relationship))->getShortName();
+
+            if (strcasecmp($shortName, $name) === 0) {
+                return $relationship;
+            }
+
+            $entityName = $relationship::getEntityName();
+
+            if ($this->normalizeRelationshipName($entityName) === $normalizedTarget) {
+                return $relationship;
+            }
+        }
+
+        return null;
+    }
+
+    protected function normalizeRelationshipName(string $name): string
+    {
+        $snake = preg_replace('/(?<!^)[A-Z]/', '_$0', $name) ?? $name;
+
+        return strtolower($snake);
     }
 }
